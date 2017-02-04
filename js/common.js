a1cc7be0cde42acfb34dcbb0376cd2f9ee473ae4
frontend-coder <verstka.shablonov@gmail.com>
2017-02-04 11:30:47 +0200

    v.0.1
    desktop версия шаблона свертана полностью, частичная адаптивность,
    всплывающее окно, меню.

---
+++ js/common.js
@@ -0,0 +1,102 @@
+$(document).ready(function() {
+	$("body").niceScroll({
+horizrailenabled:false
+});
+
+// вверхнее красиво-вращающееся меню
+// 1 и 2 строка это анимация крестика
+//3 строка - слайдер вниз меню
+$(".toggle-mnu").click(function() {
+$(this).toggleClass("on");
+$(".top-menu").slideToggle();
+return false;
+});
+
+// одинаковой высоты разные по длине ашки
+$(".name class").equalHeights();
+
+//Кнопка наверх с права от контента
+$("body").append('<div class="button-top"><i class="fa fa-angle-double-up" aria-hidden="true"></i></div>');
+// Заставляет кнопку работать как ссылку на самый вверх
+$("body").on("click", ".button-top", function() {
+	$("html, body").animate({scrollTop: 0}, "slow");
+});
+// Заставляет прятаться кнопку, если посетитель на хедере
+$(window).scroll(function() {
+if ($(this).scrollTop() > $(this).height()) {
+	$(".button-top").addClass("active");
+} else 
+{  	$(".button-top").removeClass("active");
+}
+});
+$(".owl-carouselay12").owlCarousel({
+		loop:true,
+		items: 1,
+			autoplay:true,
+	    autoplayTimeout:2500,
+	    autoplayHoverPause:true,
+
+				responsive : {
+ 0 : {
+    	nav: false,
+     margin:130
+    },
+      767 : {
+      	nav: true,
+     margin:130
+    }
+},
+
+
+		navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+		navContainer: '#customNavay12', 
+	});
+
+
+
+	$(".top-menu ul li a").mPageScroll2id({
+		 layout:"auto",
+		 offset:".top-line",
+		scrollEasing: "linear",
+		highlightByNextTarget: true,
+		keepHighlightUntilNext: true,
+		 autoScrollSpeed: true,
+		scrollSpeed : 2000
+	});
+// всплывающие окна обратной связи позвонить мне
+$("a[href='#call-back']").magnificPopup ({
+	mainClass:'my-mfp-zoom-in',
+	removalDelay:300,
+	type:'inline',
+}); 
+
+/*чтобы в формах был индивидуальный заголовок */
+$("a[href='#call-back']").click(function(){
+	var dataForm = $(this).data("form");
+	var dataText = $(this).data("text");
+	$(".forms-call h4").text(dataText);
+	$(".forms-call [name=admin-data]").val(dataForm);
+});
+
+//Аякс отправка форм Документация: http://api.jquery.com/jquery.ajax/
+
+$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			$(".forms-calldecor .success").addClass("active");
+			setTimeout(function() {
+				// Done Functions
+				$(".forms-calldecor .success").removeClass("active");
+				th.trigger("reset");
+				$.magnificPopup.close();
+			}, 3000);
+		});
+		return false;
+	});
+//ниже вставлять js код 
+
+});
---
+++ readme.txt
@@ -0,0 +1,24 @@
+
+Html5 шаблон One touch
+
+
+
+
+
+
+bootstrap сетка      v.3.3.7 
+font awesome         v.4.6.3
+Owl Carousel         v2.1.6
+jquery.js            v.2.1.3
+equalHeights.js      v.1.5.6 
+nicescroll.js        v.3.2.0
+magnific popup.js    v.1.1.0 
+PageScroll2id.js     v.1.5.6 
+common.js
+
+https://github.com/frontend-coder
+
+Спасибо за внимание к моей работе.
+
+
+
